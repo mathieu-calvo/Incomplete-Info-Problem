e138a6443be787d2d73274aaf3f72dcab8c24cef
unknown <mathieu.calvo@gmail.com>
2018-10-07 16:26:14 +0100

    initial commit from parent

---
+++ README.md
@@ -0,0 +1,2 @@
+# Incomplete-Info-Problem
+Incomplete Information Problem using Reinforcement Learning and Opponent Modelling
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pokerbot',
+      version='0.1.0',
+      description='Poket bot',
+      url='https://github.com/mathieu-calvo/Incomplete-Info-Problem',
+      author='Mathieu Calvo',
+      author_email='mathieu.calvo@edhec.com',
+      packages=['pokerbot'],
+      include_package_data=True,
+      zip_safe=False)
